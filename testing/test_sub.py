7be6bf3e133cd67f3d339160f6f3a705e51f9988
Chris KANG <chriskang@live.com>
2023-01-19 23:02:00 +0800

    Subtraction of big number as string

--- testing/test_sub.py
+++ testing/test_sub.py
@@ -8,8 +8,8 @@ class MyTestCase(unittest.TestCase):
         cases = {
             '   0123': [0, 1, 2, 3],
             '789': [0, 7, 8, 9],
-            '-123': [8, 7, 7],
-            ' -500': [5, 0, 0],
+            '-123': [9, 8, 7, 7],
+            ' -500': [9, 5, 0, 0],
             '-501': [9, 4, 9, 9]
         }
 
@@ -50,19 +50,37 @@ class MyTestCase(unittest.TestCase):
 
     def test_subtract(self):
         cases = [
-            ['123', '789'],
-            ['-2', '5'],
+            # ['123', '789'],
+            ['-2', '4'],
             ['-2', '-1'],
             ['-2', '-3'],
             ['12', '-4'],
             ['-82', '-82'],
             ['-99', '9'],
+            ['-6', '3'],
         ]
 
         for case in cases:
             expected = str(int(case[0]) - int(case[1]))
             print(f'({case[0]}) - ({case[1]}) = {expected}')
             self.assertEqual(expected, Solution.subtract(*case))
+
+    @staticmethod
+    def calc_overlap(b1: int, e1: int, b2: int, e2: int) -> int:
+        """
+        Calculate overlap of two segments
+        :param b1: begin of segment1
+        :param e1: end of segment1
+        :param b2: begin of segment2
+        :param e2: end of segment2
+        :return: length of overlap
+        """
+        if b1 > e1:
+            b1, e1 = e1, b1
+        if b2 > e2:
+            b2, e2 = e2, b2
+
+        return max(0, min(e1, e2) - max(b1, b2))
 
 
 if __name__ == '__main__':
