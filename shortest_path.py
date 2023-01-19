131438c49045c73b01c708cb6f8727d457ad65c4
Chris KANG <chriskang@live.com>
2023-01-20 01:54:10 +0800

    Dijkstra implemented by heap

--- shortest_path.py
+++ shortest_path.py
@@ -1,3 +1,4 @@
+import heapq
 from typing import Dict
 
 
@@ -27,5 +28,33 @@ class Solution:
             for vertex, dis in graph[picked].items():
                 if vertex in not_visited and not_visited[vertex] > min_dis + dis:
                     not_visited[vertex] = min_dis + dis
+
+        return result
+
+    def dijkstra_by_heap(self, graph: Dict[str, Dict[str, int]], target: str) -> Dict[str, int]:
+        if target not in graph:
+            return dict()
+        result = {target: 0}
+        # every element in heap is [distance_from_target, vertex_name]
+        heap = [[graph[target][v] if v in graph[target] else self.INT_MAX, v] for v in graph.keys() if v != target]
+        # {vertex_name: element_in_heap}
+        not_visited = {entry[1]: entry for entry in heap}
+        heapq.heapify(heap)
+
+        while not_visited:
+            # get the shortest distance
+            entry = heapq.heappop(heap)
+            min_dis, picked = entry[0], entry[1]
+
+            result[picked] = min_dis
+            del not_visited[picked]
+            # update result
+            updated = False
+            for vertex, dis in graph[picked].items():
+                if vertex in not_visited and not_visited[vertex][0] > min_dis + dis:
+                    not_visited[vertex][0] = min_dis + dis
+                    updated = True
+            if updated:
+                heapq.heapify(heap)
 
         return result
